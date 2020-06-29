5addad903669dade8d86ca16824573fcb9f48687
pernekyvirag <pernekyvirag@gmail.com>
2020-06-29 20:44:44 +0200

    Lesson 37 - The Spring Context

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,22 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		// myController is created by Spring Framework inside Spring Context
+		// ask the Context to provide me an instance of MyController bean
+		// Inversion of Control: The framework does the management of the bean
+		MyController myController = (MyController) ctx.getBean("myController");
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
 	}
 
 }
