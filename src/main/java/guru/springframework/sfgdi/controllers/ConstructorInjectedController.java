491d84628889dc774dc4c709d5e366e6646ba31c
pernekyvirag <pernekyvirag@gmail.com>
2020-07-07 19:13:20 +0200

    35 - DI using Spring

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,11 +1,14 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
 
+    // @Autowired is optional on constructors
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
