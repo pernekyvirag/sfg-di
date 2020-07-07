0fe4cfe7bdcc13e0215b043f47059cecb0baba9a
pernekyvirag <pernekyvirag@gmail.com>
2020-07-07 20:58:51 +0200

    39 - Default Profile

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -3,7 +3,8 @@ package guru.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+// default gets used when no active profile is set in the application.properties
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
 
